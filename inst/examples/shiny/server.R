74592b5515091d153a2dd65209356dbe154c42db
JJ Allaire <jj@rstudio.org>
2014-08-08 14:33:40 -0400

    react to changes in opacity

--- inst/examples/shiny/server.R
+++ inst/examples/shiny/server.R
@@ -11,13 +11,13 @@ shinyServer(function(input, output) {
     src <- c("A", "A", "A", "A", "B", "B", "C", "C", "D")
     target <- c("B", "C", "D", "J", "E", "F", "G", "H", "I")
     networkData <- data.frame(src, target)
-    simpleNetwork(networkData)
+    simpleNetwork(networkData, opacity = input$opacity)
   })
   
   output$force <- renderForceNetwork({
     forceNetwork(Links = MisLinks, Nodes = MisNodes, Source = "source",
                  Target = "target", Value = "value", NodeID = "name",
-                 Group = "group", opacity = 0.4)
+                 Group = "group", opacity = input$opacity)
   })
   
 })
--- inst/examples/shiny/ui.R
+++ inst/examples/shiny/ui.R
@@ -5,8 +5,15 @@ shinyUI(fluidPage(
   
   titlePanel("Shiny networkD3 "),
   
-  tabsetPanel(
-    tabPanel("Simple Network", simpleNetworkOutput("simple")),
-    tabPanel("Force Network", forceNetworkOutput("force"))
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("opacity", "Opacity", 0.6, min = 0.1, max = 1, step = .1)
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Simple Network", simpleNetworkOutput("simple")),
+        tabPanel("Force Network", forceNetworkOutput("force"))
+      )
+    )
   )
 ))
