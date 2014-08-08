dcb1f90ae693ed0163611792909bb0b716337df4
JJ Allaire <jj@rstudio.org>
2014-08-08 10:20:37 -0400

    add R and Rmd examples

---
+++ inst/examples/examples.R
@@ -0,0 +1,15 @@
+
+library(networkD3)
+
+# simpleNetwork
+src <- c("A", "A", "A", "A", "B", "B", "C", "C", "D")
+target <- c("B", "C", "D", "J", "E", "F", "G", "H", "I")
+networkData <- data.frame(src, target)
+simpleNetwork(networkData)
+
+# forceNetwork  
+data(MisLinks)
+data(MisNodes)
+forceNetwork(Links = MisLinks, Nodes = MisNodes, Source = "source",
+             Target = "target", Value = "value", NodeID = "name",
+             Group = "group", opacity = 0.4)
