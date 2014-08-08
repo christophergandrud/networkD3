346cdace84f1f1ecfdebd4f1ddb3e2c820a133ed
JJ Allaire <jj@rstudio.org>
2014-08-08 09:51:15 -0400

    refactor forceNetwork into common implementation

--- inst/htmlwidgets/forceNetwork.js
+++ inst/htmlwidgets/forceNetwork.js
@@ -24,6 +24,9 @@ HTMLWidgets.widget({
     
   renderValue: function(el, x, force) {
      
+    // alias options
+    var options = x.options;
+     
     // convert links and nodes data frames to d3 friendly format
     var links = HTMLWidgets.dataframeToD3(x.links);
     var nodes = HTMLWidgets.dataframeToD3(x.nodes);
@@ -31,15 +34,6 @@ HTMLWidgets.widget({
     // get the width and height
     var width = el.offsetWidth;
     var height = el.offsetHeight;
-    
-    // dispatch
-    if (x.options.zoom)
-      return this.renderZoomForce(el, width, height, links, nodes, x.options, force);
-    else
-      return this.renderForce(el, width, height, links, nodes, x.options, force);
-  },    
-    
-  renderForce: function(el, width, height, links, nodes, options, force) {
     
     var color = d3.scale.category20();
 
@@ -55,6 +49,26 @@ HTMLWidgets.widget({
   
     // select the svg element
     var svg = d3.select(el).select("svg");
+    
+    // add zooming if requested
+    if (options.zoom) {
+      svg
+        .attr("pointer-events", "all")
+        .call(d3.behavior.zoom().on("zoom", redraw));
+      
+      var vis = svg.append("svg:g");
+    
+      vis.append("svg:rect")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("fill", 'white');
+
+      function redraw() {
+        vis.attr("transform",
+          "translate(" + d3.event.translate + ")"
+          + " scale(" + d3.event.scale + ")");
+      }
+    }
     
     // draw links
     var link = svg.selectAll(".link")
@@ -123,108 +137,6 @@ HTMLWidgets.widget({
       d3.select(this).select("text").transition()
         .style("opacity", 0)
     }
-  },
-  
-  renderZoomForce: function(el, width, height, links, nodes, options, force) {
- 
-    var color = d3.scale.category20();
-
-    // create d3 force layout
-    force
-      .nodes(d3.values(nodes))
-      .links(links)
-      .size([width, height])
-      .linkDistance(options.linkDistance)
-      .charge(options.charge)
-      .on("tick", tick)
-      .start();
-  
-    // select the svg element
-    var svg = d3.select(el).select("svg")
-      .attr("pointer-events", "all")
-      .call(d3.behavior.zoom().on("zoom", redraw));
-      
-    var vis = svg
-      .append("svg:g");
-    
-    vis.append("svg:rect")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("fill", 'white');
-
-    function redraw() {
-      vis.attr("transform",
-        "translate(" + d3.event.translate + ")"
-        + " scale(" + d3.event.scale + ")");
-    }
-
-     // draw links
-    var link = svg.selectAll(".link")
-      .data(force.links())
-      .enter().append("line")
-      .attr("class", "link")
-      .style("stroke", options.linkColour)
-      .style("opacity", options.opacity)
-      .style("stroke-width", eval("(" + options.linkWidth + ")"));
-    
-    // draw nodes
-    var node = svg.selectAll(".node")
-      .data(force.nodes())
-      .enter().append("g")
-      .attr("class", "node")
-      .style("fill", function(d) { return color(d.group); })
-      .style("opacity", options.opacity)
-      .on("mouseover", mouseover)
-      .on("mouseout", mouseout)
-      .call(force.drag);
-    
-    node.append("circle")
-      .attr("r", 6)
-      .style("stroke", "#fff")
-      .style("opacity", options.opacity)
-      .style("stroke-width", "1.5px")
-      
-    node.append("svg:text")
-      .attr("class", "nodetext")
-      .attr("dx", 12)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name })
-      .style("font", options.fontsize + "px serif")
-      .style("opacity", 0)
-      .style("pointer-events", "none")
-      
-    function tick() {
-      link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-      node
-        .attr("transform", function(d) { 
-          return "translate(" + d.x + "," + d.y + ")"; 
-        });
-    }  
-      
-    function mouseover() {
-      d3.select(this).select("circle").transition()
-        .duration(750)
-        .attr("r", 16);
-      d3.select(this).select("text").transition()
-        .duration(750)
-        .attr("x", 13)
-        .style("stroke-width", ".5px")
-        .style("font", options.clickTextSize + "px serif")
-        .style("opacity", 1);
-    }
-
-    function mouseout() {
-      d3.select(this).select("circle").transition()
-        .duration(750)
-        .attr("r", 8);
-      d3.select(this).select("text").transition()
-        .style("opacity", 0)
-    }  
-  },
+  },    
 });
 
