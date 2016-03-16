45a50b7aa0d1c44e5b2698ceffaa809a6211f3c7
Casey Shannon <also.casey@gmail.com>
2016-03-15 23:47:10 -0700

    Aesthetic tweaking.

--- inst/htmlwidgets/hiveNetwork.js
+++ inst/htmlwidgets/hiveNetwork.js
@@ -15,14 +15,12 @@ HTMLWidgets.widget({
     return svg;
   },
 
-  resize: function(el, width, height, svg) {
-  /*  
-    d3.select(el).select("svg")
-        .attr("width", width)
-        .attr("height", height);
- 
-    svg.size([width, height]).resume();
-     */
+  resize: function(el, width, height, instance) {
+
+    //d3.select(el).select("svg")
+    //    .attr("width", width)
+    //    .attr("height", height + height * 0.05);
+    //this.renderValue(el, instance.x, instance);
   },
 
   renderValue: function(el, x, svg) {
@@ -31,12 +29,12 @@ HTMLWidgets.widget({
     function degrees(radians) {
       return radians / Math.PI * 180 - 90;
     }
-        
+
     // get the width and height
     var width = el.offsetWidth;
     var height = el.offsetHeight;
     var innerRadius = 40;
-    var outerRadius = 480;
+    var outerRadius = Math.min(width, height)/2 - 20;
     
     var angle = d3.scale.ordinal().domain(d3.range(4)).rangePoints([0, 2 * Math.PI]),
         radius = d3.scale.linear().range([innerRadius, outerRadius]),
@@ -76,43 +74,49 @@ HTMLWidgets.widget({
         .attr("d", d3.hive.link()
         .angle(function(d) { return angle(d.x); })
         .radius(function(d) { return radius(d.y); }))
-        .style("stroke", function(d) { return color(d.source.x); })
-        .style('stroke-width', '1.5px')
+        // .style("stroke", function(d) { return color(d.target.x); })
+        .style("stroke", "#000")
+        .style('stroke-width', '0.5px')
         .style("fill", "none");
         
     svg.selectAll(".node")
         .data(nodes)
         .enter().append("circle")
         .attr("class", "node")
-        // .attr("transform", function(d) { return "rotate(" + degrees(angle(d.x)) + ")"; })
-        // .attr("cx", function(d) { return radius(d.y); })
-        // .attr("r", 5)
-        // .style("fill", function(d) { return color(d.x); })
+        //.attr("transform", function(d) { return "rotate(" + degrees(angle(d.x)) + ")"; })
+        //.attr("cx", function(d) { return radius(d.y); })
+        //.attr("r", 5)
+        //.style("fill", function(d) { return color(d.x); })
+        .style("stroke", '#000')
+        .style('stroke-width', '2px')
         .attr("transform", function(d) { return "rotate(" + degrees(angle(d.Nodeaxis)) + ")"; })
         .attr("cx", function(d) { return radius(d.Noderadius); })
         .attr("r", function(d) { return d.Nodesize; })
         .style("fill", function(d) { return color(d.Nodecolour); })
+        
         .on("mouseover", function(d) {
-            d3.select(this)
-                .transition()
-                .duration(150)
-                .style("stroke-width", 3)
-            d3.selectAll(".link")
-                .data(links)
-                .style("stroke-width", function (dl) {
-                    if(dl.source == d){
-                        console.log(dl);
-                        return 5;
-                    }else if(dl.target == d){
-                        return 5;
-                    }
-                });
+          d3.select(this)
+            .transition()
+            .duration(150)
+            .style("stroke-width", 3)
+          
+          d3.selectAll(".link")
+            .data(linkmap)
+            .style("stroke-width", function (dl) {
+                console.log(d);
+                if(dl.source == d || dl.target == d){
+                    console.log(dl);
+                    return 5;
+                }
+            });
         })
+        
         .on("mouseout", function(){
-              d3.select(this)
-                  .style("stroke-width", 1.5)
-              d3.selectAll(".link")
-                  .style("stroke-width", 1.5)
+            d3.select(this)
+              .style("stroke-width", 1.5)
+            
+            d3.selectAll(".link")
+              .style("stroke-width", 1.5)
         });
   },
 });
