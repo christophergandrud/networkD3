990193853676b6fc946fca39093f6de447e6a51b
Maurits Evers <maurits.evers@gmail.com>
2016-06-30 11:45:57 +1000

    Fixed bug with 'value' support in igraph_to_networkD3

--- R/utils.R
+++ R/utils.R
@@ -190,7 +190,7 @@ igraph_to_networkD3 <- function(g, group, what = 'both') {
     links <- merge(links, temp_nodes, by.x = 'from', by.y = 'name')
     links <- merge(links, temp_nodes, by.x = 'to', by.y = 'name')
     if (ncol(links) == 5) {
-        links <- links[, c('id.x', 'id.y', 'value')] %>% 
+        links <- links[, c('id.x', 'id.y', 'weight')] %>% 
                       setNames(c('source', 'target', 'value'))
     }
     else {
