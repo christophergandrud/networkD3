2c4e88e5c2b0ce7a6a55ffb2f70c481bfa49ee7a
nredell <nickredell@hotmail.com>
2016-10-14 22:57:26 -0700

    Remove LinkName from tooltip when no R argument
    Final commit for this functionality...the LinkName appears in parentheses when the argument is specified and is absent when it's not.

--- inst/htmlwidgets/sankeyNetwork.js
+++ inst/htmlwidgets/sankeyNetwork.js
@@ -182,8 +182,15 @@ HTMLWidgets.widget({
             });
         // note: u2192 is right-arrow
         link.append("title")
-            .text(function(d) { return d.source.name + " \u2192 " + d.target.name +
-                "\n" + format(d.value) + " " + options.units + " (" + d.linkName + ")"; });
+            .text(function(d) {
+              if (d.linkName) {
+                return d.source.name + " \u2192 " + d.target.name +
+                "\n" + format(d.value) + " " + options.units + " (" + d.linkName + ")";
+              } else {
+                return d.source.name + " \u2192 " + d.target.name +
+                "\n" + format(d.value) + " " + options.units;
+              }
+            });
 
         node.append("rect")
             .attr("height", function(d) { return d.dy; })
