5cf6ec4d7a011fecfcb88989ed6e5dac8cd92854
CJ Yetman <cj@cjyetman.com>
2017-06-14 15:51:44 +0200

    fix elbow links in left, up, and down directions

--- inst/htmlwidgets/treeNetwork.js
+++ inst/htmlwidgets/treeNetwork.js
@@ -40,22 +40,49 @@ HTMLWidgets.widget({
         var directions = {
           "down": {
             tree: (x.options.treeType === "tidy" ? d3.tree() : d3.cluster()).size([width, height]),
-            linkgen: d3.linkVertical().x(function(d) { return d.x; }).y(function(d) { return d.y; }),
+            linkgen: (x.options.linkType === "diagonal" ?
+                        d3.linkVertical().x(function(d) { return d.x; }).y(function(d) { return d.y; })
+                      :
+                        function unobj(d) { 
+                          return d3.line().curve(d3.curveStepAfter)(Object.keys(d).map(function(key) { return [d[key].x, d[key].y]; }));
+                        }
+                      ),
             nodegen: function(d) { return "translate(" + d.x + "," + d.y + ")"; }
           },
           "left": {
-            tree: (x.options.treeType === "tidy" ? d3.tree() : d3.cluster()).size([height, width]),
-            linkgen: d3.linkHorizontal().x(function(d) { return width - d.y; }).y(function(d) { return d.x; }),
+            tree: (x.options.treeType === "tidy" ? d3.tree() : d3.cluster())
+                    .size([height, width]),
+            linkgen: (x.options.linkType === "diagonal" ?
+                        d3.linkHorizontal()
+                          .x(function(d) { return width - d.y; })
+                          .y(function(d) { return d.x; })
+                     :
+                        function unobj(d) { 
+                          return d3.line().curve(d3.curveStepBefore)(Object.keys(d).map(function(key) { return [width - d[key].y, d[key].x]; }));
+                        }),
             nodegen: function(d) { return "translate(" + (width - d.y) + "," + d.x + ")"; }
           },
           "up": {
             tree: (x.options.treeType === "tidy" ? d3.tree() : d3.cluster()).size([width, height]),
-            linkgen: d3.linkVertical().x(function(d) { return d.x; }).y(function(d) { return height - d.y; }),
+            linkgen: (x.options.linkType === "diagonal" ?
+                        d3.linkVertical().x(function(d) { return d.x; }).y(function(d) { return height - d.y; })
+                      :
+                        function unobj(d) { 
+                          return d3.line().curve(d3.curveStepAfter)(Object.keys(d).map(function(key) { return [d[key].x, height - d[key].y]; }));
+                        }
+                      ),
             nodegen: function(d) { return "translate(" + d.x + "," + (height - d.y) + ")"; }
           },
           "right": {
             tree: (x.options.treeType === "tidy" ? d3.tree() : d3.cluster()).size([height, width - 100]),
-            linkgen: d3.linkHorizontal().x(function(d) { return d.y; }).y(function(d) { return d.x; }),
+            linkgen: (x.options.linkType === "diagonal" ?
+                        d3.linkHorizontal()
+                          .x(function(d) { return d.y; })
+                          .y(function(d) { return d.x; })
+                     :
+                        function unobj(d) { 
+                          return d3.line().curve(d3.curveStepBefore)(Object.keys(d).map(function(key) { return [d[key].y, d[key].x]; }));
+                        }),
             nodegen: function(d) { return "translate(" + d.y + "," + d.x + ")"; }
           },
           "radial": {
@@ -70,25 +97,21 @@ HTMLWidgets.widget({
         // radial type needs to be centered on the canvas
         if (x.options.direction == "radial") {
           g = svg.append("g").attr("transform", "translate(" + (width / 2 + 40) + "," + (height / 2 + 90) + ")");
+          root.x0 = height / 2;
+          root.y0 = width / 2;
         } else {
           g = svg.append("g");
+          root.x0 = height / 2;
+          root.y0 = 0;
         }
 
         var tree = directions[x.options.direction].tree,
             linkgen = directions[x.options.direction].linkgen,
             nodegen = directions[x.options.direction].nodegen;
 
-        if (x.options.linkType === "elbow") {
-          var linkgen = function unobj(d) { return d3.line().curve(d3.curveStepBefore)(Object.keys(d).map(function(key) { return [d[key].y, d[key].x]; })); }
-        }
-
         var linkgrp = g.append("g").attr("class", "linkgrp");
         var nodegrp = g.append("g").attr("class", "nodegrp");
 
-        root.x0 = height / 2;
-        root.y0 = 0;
-
-        window.x = x; window.directions = directions; window.tree = tree, window.width = width; window.height = height; // for testing
         update(root);
 
         function update(source) {
@@ -116,15 +139,15 @@ HTMLWidgets.widget({
               .style("stroke-opacity", 0.4)
               .style("stroke-width", function(d){ return d.target.data.linkWidth; })
               .attr("d", function(d) {
-                var o = {x: source.x0, y: source.y0};
-                return linkgen({source: o, target: o});
-              });
+                          var o = {x: source.x0, y: source.y0};
+                          return linkgen({source: o, target: o});
+                    })
 
           // Transition links to their new position.
           var linkUpdate = linkEnter.merge(link);
           linkUpdate.transition()
               .duration(duration)
-              .attr("d", linkgen);
+              .attr("d", linkgen)
 
           // Transition exiting nodes to the parent's new position.
           link.exit().transition()
@@ -133,29 +156,25 @@ HTMLWidgets.widget({
                 var o = {x: source.x, y: source.y};
                 return linkgen({source: o, target: o});
               })
-              .remove();
+              .remove()
 
           var node = nodegrp.selectAll(".node").data(nodes, function(d,i) { return d.id || (d.id = ++i); });
 
           nodeEnter = node.enter()
               .append("g")
-              //.attr("name", function(d){ return d.data.nodeName; })
               .attr("class", "node")
               .attr("transform", function(d){ return typeof source !== 'undefined' ? nodegen({x: source.x0, y: source.y0}) : nodegen(d); })
               .on("click", click)
               .on("mouseover", mouseover)
               .on("mouseout", mouseout)
-              .attr('cursor', function(d){ return d.children || d._children ? "pointer" : "default"; });
+              .attr('cursor', function(d){ return d.children || d._children ? "pointer" : "default"; })
 
           nodeEnter.append("path")
-              //.attr("name", function(d){ return d.data.nodeName; })
-              //.style("fill", function(d){ return d._children ? d.data.nodeStroke : d.data.nodeColour;})
               .style("fill", function(d){ return d._children ? d.data.nodeStroke : 'white';})
-              //.style("fill-opacity", 1e-6)
               .style("opacity", 1e-6)
               .style("stroke", function(d){ return d.data.nodeStroke; })
               .style("stroke-width", "1.5px")
-              .attr("d", function(d){ return symbolgen(d.data.nodeSymbol).size(Math.pow(d.data.nodeSize, 2))(); });
+              .attr("d", function(d){ return symbolgen(d.data.nodeSymbol).size(Math.pow(d.data.nodeSize, 2))(); })
 
           nodeEnter.append("text")
               .attr("transform", "rotate(" + x.options.textRotate + ")")
@@ -163,47 +182,43 @@ HTMLWidgets.widget({
               .style("font-size", "1px")
               .style("opacity", function(d){ return d.data.textOpacity; })
               .style("fill", function(d){ return d.data.textColour; })
-              .text(function(d){ return d.data.name; });
+              .text(function(d){ return d.data.name; })
 
           var nodeUpdate = nodeEnter.merge(node);
 
           nodeUpdate.transition()
               .duration(duration)
-              .attr("transform", nodegen);
+              .attr("transform", nodegen)
 
           nodeUpdate.select("path")
               .transition()
               .duration(duration)
               .style("opacity", 1)
-              //.style("fill", function(d){ return d._children ? d.data.nodeStroke : d.data.nodeColour; })
               .style("fill", function(d){ return d._children ? d.data.nodeStroke : 'white'; })
-              //.style("fill-opacity", function(d){ return d._children ? 0.8 : 1e-6; })
-              //.style("fill-opacity", function(d){ return d._children ? 1 : 1; });
 
           nodeUpdate.select("text")
               .transition()
               .duration(duration)
-              .style("font-size", function(d){ return d.data.nodeFontSize + "px"; });
+              .style("font-size", function(d){ return d.data.nodeFontSize + "px"; })
 
           var nodeExit = node.exit();
 
           nodeExit.transition("exittransition")
               .duration(duration)
               .attr("transform", function(d){ return nodegen(source); })
-              .remove();
+              .remove()
 
           nodeExit.select('path')
               .transition("exittransition")
               .duration(duration)
               .style('fill', 1e-6)
               .style('fill-opacity', 1e-6)
-              //.attr("d", symbolgen().size(1e-6))
-              .attr("d", d3.symbol().type(d3.symbolCircle).size(1e-6));
+              .attr("d", d3.symbol().type(d3.symbolCircle).size(1e-6))
 
           nodeExit.select('text')
               .transition("exittransition")
               .duration(duration)
-              .style('opacity', 1e-6);
+              .style('opacity', 1e-6)
 
           nodes.forEach(function(d){
               d.x0 = d.x;
